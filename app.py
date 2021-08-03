8650e17300d1ff79b1b32646395ddc316323d7b8
Bryan Zierk <bryan.zierk@gmail.com>
2021-08-02 21:57:37 -0600

    testing

--- app.py
+++ app.py
@@ -9,13 +9,14 @@ app = Flask(__name__)
 @app.route("/image/<path:url>")
 def check(url):
 
-    org_name = url[-10:-4]
+    org_name = url[:-4]
 
     try:
         response = requests.get(url)
         img = Image.open(BytesIO(response.content)).convert("RGB")
         file_name = org_name + ".jpg"
         img.save(file_name, "jpeg")
+
         status = "Image submitted."
     except Exception as e:
         status = "Error! = " + str(e)
