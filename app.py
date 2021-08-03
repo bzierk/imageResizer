68932a9c077e477559b4db173d6805f87abfcdd8
Bryan Zierk <bryan.zierk@gmail.com>
2021-08-02 21:21:35 -0600

    Testing

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from PIL import Image
+import base64
+from io import BytesIO
+import requests
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Hello this is the new version!"
+@app.route("/image/<path:url>")
+def check(url):
+
+
+    return url
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
