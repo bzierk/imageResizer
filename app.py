faf7c843a8bdcadf673476ba096dc3e9c07be95b
Bryan Zierk <bryan.zierk@gmail.com>
2021-08-05 21:42:55 -0600

    Testing

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_file
+from flask import Flask, url_for, jsonify, send_file
 from PIL import Image
 import base64
 from io import BytesIO
@@ -7,31 +7,27 @@ import os
 
 app = Flask(__name__, static_url_path='/static')
 
-# @app.route("/<path:url>")
 @app.route("/<width>/<height>/<path:url>")
 def check(width, height, url):
 
     file_name_for_regular_data = url[-10:-4]
-    # org_name = url[:-4]
 
     try:
         response = requests.get(url)
         img = Image.open(BytesIO(response.content)).convert("RGB")
         file_name = file_name_for_regular_data + ".png"
-        img.save("/static/" + file_name, "png")
-        # response = requests.get(url)
-        # img = Image.open(BytesIO(response.content)).convert("RGB")
-        # img.thumbnail((int(width), int(height)))
-        # img.show()
-        #
-        # file_name = org_name + ".jpg"
-        # img.save(file_name, "jpeg")
-        #
-        # # return send_file(img, mimetype='image/jpg')
-        # status = file_name
+        img.thumbnail((int(width), int(height)))
+        img.save(file_name, "png")
 
-        status = "Image sent to server."
-        return status
+        # return jsonify(url_for('static', filename=file_name))
+        with open(file_name, 'rb') as image_file:
+            base64_bytes = base64.b64encode(image_file.read())
+
+        base64_string = base64_bytes.decode('utf-8')
+        json_data = {file_name: base64_string}
+
+        return jsonify(json_data)
+        # return send_file(file_name, mimetype='image/png')
     except Exception as e:
         status = "Error! = " + str(e)
 
